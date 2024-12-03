e3bbe3d6fcd4f81efc0dfe921b29674a258da8d1
hugob <1078703@hr.nl>
2024-12-03 09:42:11 +0100

    .Added migrations

---
+++ database/migrations/2024_12_03_084100_create_vacancies_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('vacancies', function (Blueprint $table) {
+            $table->id()->foreign('invatations.vacancy_id');
+            $table->string('name');
+            $table->bigInteger('location_id');
+            $table->bigInteger('salary');
+            $table->bigInteger('sector_id');
+            $table->string('description');
+            $table->bigInteger('requirements');
+            $table->boolean('hours');
+            $table->bigInteger('awaiting');
+            $table->bigInteger('wanted');
+            $table->bigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vacancies');
+    }
+};
