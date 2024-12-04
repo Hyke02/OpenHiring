69da5963b1414f6c79d2c30708387ff8eb83674c
RickDeGroot9003557 <r.degroot02@hotmail.nl>
2024-12-04 14:34:14 +0100

    fix

--- resources/views/vacatures/index.blade.php
+++ resources/views/vacatures/index.blade.php
@@ -6,8 +6,6 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-@include('layouts.navigation')
-
 <body class="overflow-x-hidden">
 
 <div class="container mx-auto mt-8 max-w-full">
