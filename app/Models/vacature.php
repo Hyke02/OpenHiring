dc1e9722d43f7611b621ee8428809829624ad180
jonah <jonahbeijer@gmail.com>
2024-12-04 10:26:59 +0100

    eerste push

---
+++ app/Models/vacature.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class vacature extends Model
+{
+    protected $fillable = [
+
+    ];
+
+    public function sector()
+    {
+        return $this->belongsTo(Sector::class);
+    }
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'users_id');
+    }
+
+
+
+
+}
+
+
+
+
