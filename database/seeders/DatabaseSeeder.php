d618fd26dfa90c564b9cd221dd9d52d21b7d226f
hugob <1078703@hr.nl>
2024-12-05 11:55:28 +0100

    Added dummydata

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Sector;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\vacancy;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(1)->create();
+        Sector::factory(10)->create();
+        Vacancy::factory(10)->create();
 
 //        User::factory()->create([
 //            'name' => 'Test User',
