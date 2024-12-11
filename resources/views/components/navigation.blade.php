83d193964d12b240d13c66507ae88140c2680850
Maaike <maaike.moeliker@gmail.com>
2024-12-11 11:40:49 +0100

    home page

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,15 +1,15 @@
 <div class="flex relative p-6">
-    <a href="{{ route('home') }}" class="w-">
-        <img src="{{ asset('storage/images/OpenHiring-logo.svg') }}" alt="logo van open hiring" class="w-12 focus:border-indigo-600">
+    <a href="{{ route('home') }}" class="w-16">
+        <img src="{{ asset('storage/images/logo-oh.png') }}" alt="logo van open hiring" class="w-17 focus:border-indigo-600">
     </a>
 
     <div class="burger ml-auto text-[48px]">
         <img src="{{ asset('storage/images/burger-menu.svg') }}" alt="menu icoon" class="w-12" fill="currentcolor">
     </div>
 
-    <div class="dropdown opacity-0 scale-y-0 transform origin-top absolute left-0 right-0 top-full
-    mt-2 bg-white min-h-screen transition-all duration-300 pt-[20%]">
-        <nav class="text-center text-4xl flex flex-col space-y-10">
+    <div class="dropdown opacity-0 scale-y-0 transform origin-top absolute left-0 right-0 top-full mt-4
+                bg-white shadow-lg rounded-lg min-h-[70vh] w-full max-w-full mx-0 transition-all duration-300 pt-[15%] z-40">
+        <nav class="text-center text-3xl flex flex-col space-y-6 p-8">
             <x-nav-link href="/profiel">Profiel</x-nav-link>
             <x-nav-link href="/mijn-vacatures">Mijn Vacatures</x-nav-link>
             <x-nav-link href="/vacatures">Vacatures</x-nav-link>
@@ -32,4 +32,3 @@
         }
     });
 </script>
-
