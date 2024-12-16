5471feac74167ea4dcc804728453bdfd80f6b519
RickDeGroot9003557 <r.degroot02@hotmail.nl>
2024-12-16 14:04:17 +0100

    nav bar made sticky, progress bars added and amount of info shown reduced

--- resources/views/components/info-icon.blade.php
+++ resources/views/components/info-icon.blade.php
@@ -1,24 +1,42 @@
-<div class="">
-    <button id="info-icon" class="z-10 m-[10px] group">
+<div class="flex items-center space-x-4 w-full pr-[1.5rem]">
+    <button id="info-icon" class="z-20 m-[10px] group">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="1" width="50" height="50">
             <circle cx="12" cy="12" r="10" stroke="currentColor" class="group-hover:stroke-[#AA0061]" />
             <line x1="12" y1="10" x2="12" y2="18" stroke="currentColor" stroke-width="1" class="group-hover:stroke-[#AA0061]" />
             <circle cx="12" cy="7" r="1" fill="currentColor" class="group-hover:stroke-[#AA0061]" />
+            <span>Help</span>
         </svg>
     </button>
 
-    <div id="info-box" class="fixed inset-0 bg-white text-2xl opacity-0 scale-y-0 transform origin-top transition-all duration-300 p-5 overflow-y-auto">
-        <div class="text-center">
-            <h2 class="text-3xl font-bold mb-4">Zo solliciteer je</h2>
-            <x-info-content-box>content</x-info-content-box>
-            <button
-                id="close-info"
-                class="text-2xl bg-[#AA0061] text-white px-4 py-4 rounded-md shadow hover:bg-white hover:text-[#AA0061] hover:border-[#AA0061] border-2 border-transparent mt-5 w-full">
-                sluit
-            </button>
-        </div>
+    @php
+        $progressAmount = [
+            'vacancy.index' => 'w-[25%]',
+            'vacancy.show' => 'w-[50%]',
+            'my-vacancy.index' => 'w-[75%]',
+            'invitation.index' => 'w-[100%]',
+        ];
+        $currentProgress = $progressAmount[Route::currentRouteName()] ?? 'w-0';
+    @endphp
+
+    <div id="info-bar" class="relative w-full h-4 bg-gray-200 rounded-lg overflow-hidden">
+        <div class="h-full bg-[#AA0061] transition-all duration-300 {{ $currentProgress }}"></div>
     </div>
 </div>
+
+<div id="info-box" class="fixed inset-0 bg-white text-2xl opacity-0 scale-y-0 transform origin-top transition-all duration-300 p-5 overflow-y-auto z-30">
+    <div class="text-center">
+        <h2 class="text-3xl font-bold mb-4">Zo solliciteer je</h2>
+        <x-info-content-box>content</x-info-content-box>
+        <button
+            id="close-info"
+            class="text-2xl bg-[#AA0061] text-white px-4 py-4 rounded-md shadow hover:bg-white hover:text-[#AA0061] hover:border-[#AA0061] border-2 border-transparent mt-5 w-full">
+            sluit
+        </button>
+    </div>
+</div>
+
+
+
 
 
 <script>
