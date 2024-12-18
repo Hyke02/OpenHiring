45d447901312825b72b15c650d7d06b1aab256bf
jonah <jonahbeijer@gmail.com>
2024-12-18 13:39:10 +0100

    translate bijna klaar

--- resources/views/vacancy/index.blade.php
+++ resources/views/vacancy/index.blade.php
@@ -15,10 +15,9 @@
 <x-navigation class="z-20"></x-navigation>
 <x-info-icon>placeholder text</x-info-icon>
 <div class="container mx-auto mt-8 max-w-full z-50">
-
-
 </div>
-<div class="mx-auto mt-8 max-w-full z-10">
+
+<div class="mx-auto mt-8 max-w-full z-10 ">
     <form method="GET" action="{{ route('vacancy.index') }}" class="mb-8">
         <div class="flex gap-4 items-center mb-4 mx-4">
             <div class="flex-1">
@@ -55,17 +54,16 @@
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 m-4">
         @foreach ($vacancies as $vacancy)
             <div class="bg-white shadow rounded-lg overflow-hidden border border-[#444343]">
-                <h1 class="font-black text-2xl px-4 pt-5 vacancy-title">{{ $vacancy->job_title }}</h1>
-                <h2 class="font-light text-lg font px-4 vacancy-company no-translate">{{ $vacancy->company_name }}
-                    - {{ $vacancy->location->name }}</h2>
+                <h1 class="font-black text-2xl px-4 pt-5 vacancy-title">{{ $vacancy->name }}</h1>
+                <h2 class="font-light text-lg font px-4 vacancy-company no-translate vacancy-location no-translate">{{ $vacancy->company_name }} - {{ $vacancy->location->location }}</h2>
 
                 <div class="flex flex-row justify-between p-4">
                     <div class=" flex flex-col gap-2 mb-2 lg:mb-0 w-full lg:w-2/3">
                         <div class="flex mt-2" >
-                             <img src="{{ asset('storage/images/8665257_clock_watch_icon.svg') }}" alt="Clock Icon" class="w-7 h-auto mr-2 mt-1">
-                             <div class="bg-[#f2fade] shadow-[0_0_0_1.5px_#dee8ba] rounded-full p-3 inline-flex items-center w-fit">
-                                 <p class=" m-0 text-xs lg:text-sm"> {{ $vacancy->hours }} uur per week </p>
-                             </div>
+                            <img src="{{ asset('storage/images/8665257_clock_watch_icon.svg') }}" alt="Clock Icon" class="w-7 h-auto mr-2 mt-1">
+                            <div class="bg-[#f2fade] shadow-[0_0_0_1.5px_#dee8ba] rounded-full p-3 inline-flex items-center w-fit">
+                                <p class=" m-0 text-xs lg:text-sm"> {{ $vacancy->hours }} uur per week </p>
+                            </div>
                         </div>
                         <div class="flex mt-2">
                             <img src="{{ asset('storage/images/3669346_ic_symbol_euro_icon.svg') }}" alt="Euro Icon" class="w-8 h-auto mr-2">
@@ -73,12 +71,12 @@
                                 <p>{{ $vacancy->salary }} euro per maand</p>
                             </div>
                         </div>
-                            <div class="flex mt-2">
-                                <img src="{{ asset('storage/images/9004762_search_find_zoom_magnifier_icon.svg') }}" alt="Search Icon" class="w-8 h-auto mr-2">
-                                <div class="bg-[#f2fade] shadow-[0_0_0_1.5px_#dee8ba] rounded-full p-3 text-xs lg:text-sm w-fit">
-                                    <p>{{ $vacancy->wanted }} nodig</p>
-                                </div>
+                        <div class="flex mt-2">
+                            <img src="{{ asset('storage/images/9004762_search_find_zoom_magnifier_icon.svg') }}" alt="Search Icon" class="w-8 h-auto mr-2">
+                            <div class="bg-[#f2fade] shadow-[0_0_0_1.5px_#dee8ba] rounded-full p-3 text-xs lg:text-sm w-fit">
+                                <p>{{ $vacancy->wanted }} nodig</p>
                             </div>
+                        </div>
                         <div class="flex mt-2">
                             <img src="{{ asset('storage/images/8541636_clipboard_list_icon.svg') }}" alt="Clipboard Icon" class="w-8 h-auto mr-2">
                             <div class="flex gap-2">
@@ -87,10 +85,11 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="flex justify-center lg:justify-end items-center w-full lg:w-1/3">
-                            <img src="{{ asset('storage/'. $vacancy->logo) }}" alt="{{ $vacancy->company_name }} logos -l public/storage" class="max-w-[145px] max-h-[145px] object-cover rounded-md">
-                        </div>
                     </div>
+                    <div class="flex justify-center lg:justify-end items-center w-full lg:w-1/3">
+                        <img src="{{ asset('storage/' . $vacancy->images) }}" alt="{{ $vacancy->vacancy_name }}" class="max-w-[145px] max-h-[145px] object-cover rounded-md">
+                    </div>
+                </div>
 
                 <div class="flex justify-between m-4">
                     <x-sub-button href="{{ route('vacancy.show', $vacancy->id) }}" class="button !py-3 !px-4 !text-base mb-5">
@@ -106,14 +105,11 @@
         @endforeach
     </div>
 </div>
-<x-footer />
-
 
 </body>
 </html>
 
 <script>
-
     const voiceMapping = {
         nl: "Dutch Male",
         en: "UK English Male",
@@ -125,16 +121,13 @@
         el: "Greek Male",
         hu: "Hungarian Male"
     };
-
     function readJobDetails(button) {
         const parent = button.closest('.bg-white');
         const headings = parent.querySelectorAll('h1, h2');
         const paragraphs = parent.querySelectorAll('p');
-
         // Haal de geselecteerde taal op uit de dropdown
         const languageCode = document.getElementById('language-selector').value;
         const voice = voiceMapping[languageCode];
-
         let text = '';
         headings.forEach(heading => {
             text += heading.textContent + ' ';
@@ -142,7 +135,6 @@
         paragraphs.forEach(paragraph => {
             text += paragraph.textContent + ' ';
         });
-
         if (text) {
             responsiveVoice.speak(text, voice);
         }
